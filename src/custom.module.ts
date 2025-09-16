e13d2e7f6ef57a1c5676897fcd172a3de3695ce6
Visal Suwanarat <visal@amitysolutions.com>
2025-09-16 21:12:02 +0700

    chore: rename file

--- src/custom.module.ts
+++ src/custom.module.ts
@@ -1,4 +1,5 @@
+import { AuthModule } from './modules/auth/auth.module';
 import { CatModule } from './modules/cat/cat.module';
 import { ResponseBuilderModule } from './modules/response-builder/response-builder.module';
 
-export const customModules = [ResponseBuilderModule, CatModule];
+export const customModules = [AuthModule, ResponseBuilderModule, CatModule];
